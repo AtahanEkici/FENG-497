23b2f18f4aaf8c452ef4034cf4b1d8058db0bb16
Ata <laythese@gmail.com>
2021-01-07 19:49:44 +0300

    Add files via upload

---
+++ ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+	public Text textScore;
+	public int current_score;
+
+	void Start () 
+	{
+		textScore.text = "Score: 0";
+		current_score = 0;
+	}
+
+	public int getScore()
+    {
+		return current_score;
+    }
+	
+	public void UpdateScore (int score) 
+	{
+		current_score = (score / 3) * 10;
+		textScore.text = "Score: " + current_score;
+	}
+}
